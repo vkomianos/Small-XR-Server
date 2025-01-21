5a11e2793cc9d6824bd2144f8674deccca5b5b92
Vasileios Komianos <vkomianos@ionio.gr>
2025-01-21 22:34:28 +0200

    First basic version of server and a testing client

--- server/index.php
+++ server/index.php
@@ -1,7 +1,25 @@
 <?php
 
+
+
+/* Addition for localhost served apps -> */
+header("Access-Control-Allow-Origin: *");
+header('Access-Control-Allow-Methods: PUT, GET, POST');
+header("Access-Control-Allow-Headers: X-Requested-With, Content-Type, Accept");
+/* <- Addition for localhost served apps */
+
+
 require_once "./SleekDB-master/src/Store.php";
 
 //echo "OK";
+
+$storePositions = new \SleekDB\Store("positions", "DB");
+
+
+//$position = ["userID" => "123", "position" => "x y z"];
+$position = ["userID" => $_POST['userID'], "position" => $_POST['position']];
+
+
+$result = $storePositions->insert($position);
 
 ?>
