3d52223a508383b8576b887005ed623dab0d9270
vkomianos <vkomianos@ionio.gr>
2025-01-26 00:15:39 +0200

    Login functionality
    Users should be allowed to login and define their usernames.

--- server/index.php
+++ server/index.php
@@ -2,6 +2,11 @@
 
 error_reporting(E_ERROR | E_PARSE);
 
+session_start();
+//print_r($_SESSION);
+
+
+
 /* Addition for localhost served apps -> */
 header("Access-Control-Allow-Origin: *");
 header('Access-Control-Allow-Methods: PUT, GET, POST');
@@ -10,6 +15,12 @@ header("Access-Control-Allow-Headers: X-Requested-With, Content-Type, Accept");
 
 
 require_once "./SleekDB-master/src/Store.php";
+
+if (!isset($_SESSION['user-id']))
+{
+	echo "No session.<br/>";
+}
+
 
 //echo "OK";
 $userID = "";
@@ -22,9 +33,58 @@ $command = "";
 
 //print_r($_POST);
 
+$_POST['command'] = "login";
 if (isset($_POST['command']) || isset($_POST["userID"]))
 {
 	//echo "Good";
+	
+	$_POST['command'] = "login";
+	$_POST['username'] = 'vkom';
+	$_POST['password'] = '1234';
+	
+	if ($_POST['command'] == "login" && isset($_POST['username']) && isset($_POST['password']))
+	{
+		$storeUsers = new \SleekDB\Store("users", "DB");
+		
+		$userNameExists = $storeUsers->createQueryBuilder()
+		->where([ "username", "=", $_POST['username'] ])
+		->getQuery()->exists();
+		
+		if ($userNameExists)
+		{
+			// check password to login
+			$user = $storeUsers->createQueryBuilder()
+			->where(
+			[
+				["username", "=", $_POST['username']],
+				["password", "=", $_POST['password']]
+			])->getQuery()->exists();
+			
+			//echo $user;
+			echo "Check login ";
+			echo $user == "1" ? "true" : "false";
+			echo ".<br/>";
+			
+			if ($user == 1)
+			{
+				$_SESSION['username'] = $_POST['username'];
+				$_SESSION['session_token'] = bin2hex(random_bytes(32));
+				
+				// save session_token in DB and check on every request
+				
+				print_r($_SESSION);
+			}
+			
+		}
+		else
+		{
+			// create user and proceeed
+			$createUser = ["username" => $_POST['username'], "password" => $_POST['password']];
+			$result = $storeUsers->insert($createUser);
+			
+			echo "Create user ".print_r($result);
+		}
+	}
 	
 	if ($_POST['command'] == "update")
 	{
